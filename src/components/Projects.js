f64c11724d7c434a4249d02ad1b89b8a14ff0a07
BlurryBunny <CoChiCoriginal@gmail.com>
2023-09-08 08:14:07 -0600

    (update) update profile to 2023 August

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,14 @@ import { Row, Container, Col, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import Nav from 'react-bootstrap/Nav';
 import project_img_1 from '../assets/img/onWeb_ControlEscolar.jpg';
-import project_img_1_1 from '../assets/img/onWeb_ControlEscolar_2.jpg';
+import doMas_img_1 from '../assets/img/onWeb_DoMas_Layer.png';
+
+import redom_img_1 from '../assets/img/onWeb_R8_PWA_Customer.png';
+import redom_img_2 from '../assets/img/onWeb_R8_Web_Shop.png';
+import redom_img_3 from '../assets/img/onWeb_R8_Mobile_App_Ambassor.png';
+
+import cruciart_img_1 from '../assets/img/onWeb_curciart.png';
+
 
 import project_img_2 from '../assets/img/onWeb_creandoHeroes.jpg';
 import project_img_3 from '../assets/img/onWeb_PortalAgenda.jpg';
@@ -14,13 +21,41 @@ import my_project_img_3 from '../assets/img/projects_MexAnt.jpg';
 
 
 
-import colorSharp2 from '../assets/img/color-sharp2.png';
-
-
-
 export const Projects = () => {
-
+    
     const onWeb = [
+        {
+            title: "Curciart",
+            description: "The RedOm8 mobile application is designed for internal sellers who pretend to offer service to customers, assigning balance with multiple payment methods, adding more actors as customers or sellers to the network, all from a single application. Created with Flutter, Firebase, Odoo, Python",
+            imgUrl: cruciart_img_1,
+            projectUrl: "https://curciart.com/",
+        },
+        {
+            title: "RedOm8 Mobile App",
+            description: "The RedOm8 mobile application is designed for internal sellers who pretend to offer service to customers, assigning balance with multiple payment methods, adding more actors as customers or sellers to the network, all from a single application. Created with Flutter, Firebase, Odoo, Python",
+            imgUrl: redom_img_3,
+            projectUrl: "https://redom8.com/",
+        },
+        {
+            title: "Do Mas",
+            description: "Do Mas is a mobile focused technology solution to provide for the end user a pet products store, also the sellers can control the products and orders. Created with Flutter, Firebase.",
+            imgUrl: doMas_img_1,
+            projectUrl: "https://play.google.com/store/apps/details?id=com.doMas.android.abc",
+        },
+        {
+            title: "Ciudadanos RedOm8",
+            description: "The RedOm8 Citizens application based on the PWA (Progressive Web App) structure offers end users, i.e. those who use the telephone service, a platform to monitor their usage, acquire services, register new lines or check their history of purchases. Created with Quasar, VueJS, Firebase, Odoo, Python.",
+            imgUrl: redom_img_1,
+            projectUrl: "https://ciudadanos.vercel.app/#/",
+        },
+        {
+            title: "RedOm8",
+            description: "The RedOm8 web platform is the main reference of the company where the service is described, its services can be purchased and the other tools of the ecosystem can be visualized. Created with NextJs, Firebase, Odoo and Python.",
+            imgUrl: redom_img_2,
+            projectUrl: "https://redom8.com/",
+        },
+        
+
         {
             title: "Control Escolar",
             description: "A control system for postgraduate programs taught at the UASLP, it is used by all stakeholders involved in the educational process.",
@@ -79,10 +114,10 @@ export const Projects = () => {
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
-                                    <Nav.Link eventKey="first">On the web</Nav.Link>
+                                    <Nav.Link eventKey="first">Job Collaborations</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="second">My projects</Nav.Link>
+                                    <Nav.Link eventKey="second">Personal proyects</Nav.Link>
                                 </Nav.Item>
                             </Nav>
 
